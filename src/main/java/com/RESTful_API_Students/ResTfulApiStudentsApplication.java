66c5fb6b322f41e42e37df4014be63f1d24a8239
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-12-19 15:58:16 -0500

    feat: initial commit

---
+++ src/main/java/com/RESTful_API_Students/ResTfulApiStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.RESTful_API_Students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulApiStudentsApplication.class, args);
+	}
+
+}
