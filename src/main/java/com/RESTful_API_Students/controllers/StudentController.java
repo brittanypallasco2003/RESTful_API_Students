5ec55180dd828ee5d57a694e13f2b1d5ca25331a
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-12-19 16:34:59 -0500

    feat: Creating methods for HTTP GET requests

--- src/main/java/com/RESTful_API_Students/controllers/StudentController.java
+++ src/main/java/com/RESTful_API_Students/controllers/StudentController.java
@@ -1,8 +1,7 @@
 package com.RESTful_API_Students.controllers;
 
 import com.RESTful_API_Students.domain.Student;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,12 +12,32 @@ import java.util.List;
 @RequestMapping("/students")
 public class StudentController {
 
-    List <Student> students= new ArrayList<>(Arrays.asList(
-            new Student(123,"Juan Lopéz","juan@gmail.com", 18, "Algebra lineal"),
-            new Student(124, "Enrique García", "enri@gmail",19, "Estadística"),
-            new Student(124, "Felipe García", "felipe@gmail",19, ""),
-            new Student(124, "Daniel Flores", "dani@gmail",19, "")
+    private List<Student> students = new ArrayList<>(Arrays.asList(
+            new Student(123, "Juan Lopéz", "juan@gmail.com", 18, "Algebra lineal"),
+            new Student(124, "Enrique García", "enri@gmail", 19, "Estadística"),
+            new Student(125, "Felipe García", "felipe@gmail", 23, "French"),
+            new Student(126, "Daniel Flores", "dani@gmail", 24, "Computing")
     ));
+
+    @GetMapping
+    public List<Student> showStudents() {
+        return students;
+    }
+
+    @GetMapping("/{email}")
+    public Student showStudent(@PathVariable String email) {
+        for (Student st : students) {
+            if (st.getEmail().equalsIgnoreCase(email)) {
+                return st;
+            }
+        }
+        return null;
+    }
+
+//    @PostMapping
+//    public Student createStudent() {
+//
+//    }
 
 
 }
